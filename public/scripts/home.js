4d492d743d9ee2f0e849cf48c4d5cf8b0f9ca28b
Swordax <88690330+SwordaxSy@users.noreply.github.com>
2022-05-10 21:48:41 +0400

    Add files via upload

--- public/scripts/home.js
+++ public/scripts/home.js
@@ -1,16 +1,3 @@
-// scroller
-const scrollerUp = document.querySelector(".scroller-up");
-const scrollerDown = document.querySelector(".scroller-down");
-
-function scrollFunction(updown) {
-    const pageHeight = window.innerHeight;
-    let amount = updown ? -pageHeight : pageHeight;
-    window.scrollBy(0, amount);
-}
-
-scrollerUp.addEventListener("click", () => scrollFunction(true));
-scrollerDown.addEventListener("click", () => scrollFunction(false));
-
 // @ copy tooltip
 const atElement = document.querySelector(".contacts-at");
 const tooltip = document.querySelector(".tooltip");
